84f5ad6b1a88ee9be427ccb8c13e05f3b3017d6c
ayushdhiman-py <ayushdhiman0107@gmail.com>
2023-01-15 20:10:04 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import ChatPage from './pages/ChatPage';
+import Homepage from './pages/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/chats' element={<ChatPage/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
